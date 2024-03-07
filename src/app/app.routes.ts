1de9c6d61bfd5d5f2498968c60dbcf8596182fa7
Dikendev <diken.dev@gmail.com>
2024-03-06 23:38:49 -0300

    feat add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ChartComponent } from './external/charts/chart.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'chart',
+    component: ChartComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
